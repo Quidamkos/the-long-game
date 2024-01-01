da2ea7333c2d27ce66c8086ebab172c7507a11b5
quidamk <quidam.rk@gmail.com>
2024-01-01 03:46:28 +0100

    Commit authentification no css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Navbar from './components/Navbar'
+import SignUpModal from "./components/SignUpModal";
+import SignInModal from "./components/SignInModal"
+import Private from "./pages/Private/Private"
+import PrivateHome from "./pages/Private/PrivateHome/PrivateHome";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    // Étape 1 : Initialisation des données
+  /*const storedSettings = JSON.parse(localStorage.getItem('player')) || {};
+  const storedRanks = JSON.parse(localStorage.getItem('ranks')) || { rank: [] };
+  const defaultPhoto = localStorage.getItem('picture') || personna;
+
+  // Étape 2 : Utilisation de useState
+  const [userData, setUserData] = useState({
+    birth: storedSettings.birth || '',
+    nickname: storedSettings.nickname || '',
+    experience: storedSettings.experience || 0,
+    money: storedSettings.money || 0,
+    rank: storedRanks.rank,
+    imageBase64: defaultPhoto,
+  });
+
+  // Étape 3 : Synchronisation avec useEffect
+  useEffect(() => {
+    localStorage.setItem('player', JSON.stringify({
+      birth: userData.birth,
+      nickname: userData.nickname,
+      experience: userData.experience,
+      money: userData.money
+    }));
+    localStorage.setItem('ranks', JSON.stringify({ rank: userData.rank }));
+  }, [userData]);
+
+  // Fonction pour mettre à jour userData
+  const updateUserData = (newData) => {
+    setUserData(prevUserData => ({ ...prevUserData, ...newData }));
+  };*/
+
+    return (
+      <>
+        <SignUpModal />
+        <SignInModal />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/private" element={<Private />}>
+            <Route path="/private/private-home" element={<PrivateHome />} />
+          </Route>
+        </Routes>
+      </>
+    );
+  }
+  
+  export default App;
---
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function PrivateHome() {
+    return (
+        <div>
+            <p>Tu as ton compte !</p>
+        </div>
+    )
+}
